c8d29f7b6c6a5960d6de49cc8ceca345d1786626
ta-02 <talal.gl2004@gmail.com>
2024-07-20 19:01:03 -0400

    added a todo

---
+++ README.md
@@ -0,0 +1,6 @@
+## TODO
+
+- [ ] Implement authorized routes
+- [ ] Add in Tanstack Query
+- [ ] Implement data fetching from instagramp API
+- [ ] Add in Profile Page
--- frontend/src/routes/profile.tsx
+++ frontend/src/routes/profile.tsx
@@ -3,4 +3,3 @@ import { createFileRoute } from "@tanstack/react-router";
 export const Route = createFileRoute("/profile")({
   component: () => <div>Hello profile!</div>,
 });
-
