d110b5bffdb4a8137d9afc41413bd5541cc668e1
ta-02 <talal.gl2004@gmail.com>
2024-07-20 20:15:33 -0400

    added react query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { routeTree } from "./routeTree.gen";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
 const router = createRouter({ routeTree });
+
+const queryClient = new QueryClient();
 
 declare module "@tanstack/react-router" {
   interface Register {
@@ -17,7 +20,9 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </StrictMode>,
   );
 }
--- frontend/src/routes/profile.tsx
+++ frontend/src/routes/profile.tsx
@@ -1,5 +1,31 @@
+import { useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
+import { UserType } from "@kinde-oss/kinde-typescript-sdk";
+import axios from "axios";
 
 export const Route = createFileRoute("/profile")({
-  component: () => <div>Hello profile!</div>,
+  component: ProfilePage,
 });
+
+const getUserData = async () => {
+  const res = await axios.get("/api/me");
+  return res.data.user;
+};
+
+function ProfilePage() {
+  const { isPending, error, data } = useQuery<UserType>({
+    queryKey: ["getUserData"],
+    queryFn: getUserData,
+  });
+
+  if (isPending) return "Loading...";
+  if (error) return "An error has occurred: " + error.message;
+
+  const user = data;
+
+  return (
+    <div>
+      <div>Hello {user.given_name}</div>
+    </div>
+  );
+}
